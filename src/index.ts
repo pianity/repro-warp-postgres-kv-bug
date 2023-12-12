61d5d62ef52451b6241249a9988e05c769d0156f
Eyal Chojnowski <eyal.choj@gmail.com>
2023-12-12 18:19:20 +0100

    :sparkles: Isolate bug and re-implement minimal reproduction

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import ArLocal from "arlocal";
-import { SmartWeaveGlobal, LoggerFactory, WarpFactory, Transaction } from "warp-contracts";
+import { SmartWeaveGlobal, LoggerFactory, WarpFactory, Contract } from "warp-contracts";
 import { DeployPlugin, ArweaveSigner } from "warp-contracts-plugin-deploy";
 import { PgSortKeyCache, PgSortKeyCacheOptions } from "warp-contracts-postgres";
 
@@ -7,79 +6,47 @@ LoggerFactory.INST.logLevel("error");
 
 let SmartWeave: SmartWeaveGlobal;
 
-type Erc20Action =
-    | { function: "fundMe" }
-    | { function: "transfer"; from?: string; target: string; quantity: number }
-    | { function: "balanceOf"; target?: string };
+type ContractAction =
+    | { function: "setMessage"; message: string }
+    | { function: "removeMessage" }
+    | { function: "getMessage" }
+    | { function: "getMessageMap" };
 
-const erc20Contract = (() => {
-    async function handle(
-        state: unknown,
-        { input, caller }: { input: Erc20Action; caller: string },
-    ): Promise<{ state: unknown } | { result: unknown }> {
-        if (input.function === "fundMe") {
-            const newBalance = (((await SmartWeave.kv.get(caller)) as number) ?? 0) + 1_000_000;
-            await SmartWeave.kv.put(caller, newBalance);
-
-            return { state };
-        }
-
-        if (input.function === "balanceOf") {
-            const target = input.target ?? caller;
-            const balance = ((await SmartWeave.kv.get(target)) as number) ?? 0;
-
-            return { result: [target, balance] };
-        }
-
-        if (input.function === "transfer") {
-            const owner = input.from ?? caller;
-            const ownerBalance = ((await SmartWeave.kv.get(owner)) as number) ?? 0;
-            if (ownerBalance < input.quantity) {
-                throw new Error("Not enough balance");
-            }
-            const newOwnerBalance = ownerBalance - input.quantity;
-            if (newOwnerBalance === 0) {
-                await SmartWeave.kv.del(owner);
-            } else {
-                await SmartWeave.kv.put(owner, newOwnerBalance);
-            }
-
-            const newTargetBalance =
-                (((await SmartWeave.kv.get(input.target)) as number) ?? 0) + input.quantity;
-            await SmartWeave.kv.put(input.target, newTargetBalance);
-
-            return { state };
-        }
-
-        throw new Error("Unknown function");
-    }
-    return handle.toString();
-})();
-
-type ProxyAction = { function: "transfer"; erc20: string; target: string; quantity: number };
-
-const proxyContract = (() => {
-    async function handle(
-        state: unknown,
-        action: { input: ProxyAction; caller: string },
-    ): Promise<{ state: unknown } | { result: unknown }> {
-        if (action.input.function === "transfer") {
-            const transferInput: Erc20Action = {
-                function: "transfer",
-                from: action.caller,
-                target: action.input.target,
-                quantity: action.input.quantity,
-            };
-            await SmartWeave.contracts.write(action.input.erc20, transferInput);
-
-            return { state };
-        }
-
-        throw new Error("Unknown function");
+const contractSrc = async function handle(
+    state: unknown,
+    { input, caller: _caller }: { input: ContractAction; caller: string },
+): Promise<{ state: unknown } | { result: unknown }> {
+    if (input.function === "setMessage") {
+        await SmartWeave.kv.put("message", input.message);
+        return { state };
     }
 
-    return handle.toString();
-})();
+    if (input.function === "removeMessage") {
+        await SmartWeave.kv.del("message");
+        return { state };
+    }
+
+    if (input.function === "getMessage") {
+        const message = await SmartWeave.kv.get("message");
+        return { result: message };
+    }
+
+    if (input.function === "getMessageMap") {
+        const message = await SmartWeave.kv.kvMap();
+        return { result: message };
+    }
+
+    throw new Error("Unknown function");
+};
+
+async function readValues(contract: Contract) {
+    console.log(
+        (await contract.viewState({ function: "getMessage" } satisfies ContractAction)).result,
+    );
+    console.log(
+        (await contract.viewState({ function: "getMessageMap" } satisfies ContractAction)).result,
+    );
+}
 
 (async () => {
     const cacheOpts = (tableName: string): PgSortKeyCacheOptions => ({
@@ -93,92 +60,59 @@ const proxyContract = (() => {
         maxEntriesPerKey: 10000,
     });
 
-    const warp = WarpFactory.forTestnet({ inMemory: true, dbLocation: "./cache/warp" }).use(
-        new DeployPlugin(),
-    );
-    // .useKVStorageFactory((contractTxId) => new PgSortKeyCache(cacheOpts(contractTxId)));
+    const warp = WarpFactory.forTestnet({ inMemory: true, dbLocation: "./cache/warp" })
+        .use(new DeployPlugin())
+        .useKVStorageFactory((contractTxId) => new PgSortKeyCache(cacheOpts(contractTxId)));
 
     const { arweave } = warp;
 
     const wallet1 = await arweave.wallets.generate();
-    const wallet1Addr = await arweave.wallets.jwkToAddress(wallet1);
     const wallet1Signer = new ArweaveSigner(wallet1);
 
-    const erc20TxId = (
+    const contractTxId = (
         await warp.deploy({
             wallet: wallet1Signer,
-            initState: JSON.stringify({}),
-            src: erc20Contract,
+            initState: "{}",
+            src: contractSrc.toString(),
             evaluationManifest: {
                 evaluationOptions: {
-                    internalWrites: true,
                     useKVStorage: true,
                 },
             },
         })
     ).contractTxId;
-    const erc20 = warp
-        .contract(erc20TxId)
-        .setEvaluationOptions({ internalWrites: true, useKVStorage: true })
+    const contract = warp
+        .contract(contractTxId)
+        .setEvaluationOptions({ useKVStorage: true })
         .connect(wallet1);
 
-    const proxyTxId = (
-        await warp.deploy({
-            wallet: wallet1Signer,
-            initState: JSON.stringify({}),
-            src: proxyContract,
-            evaluationManifest: {
-                evaluationOptions: {
-                    useKVStorage: true,
-                    internalWrites: true,
-                },
-            },
-        })
-    ).contractTxId;
-    const proxy = warp
-        .contract(proxyTxId)
-        .setEvaluationOptions({ internalWrites: true, useKVStorage: true })
-        .connect(wallet1);
+    console.log("=== initial ===");
 
-    console.log((await erc20.viewState({ function: "balanceOf" } satisfies Erc20Action)).result);
-    console.log(
-        (await erc20.viewState({ function: "balanceOf", target: "bob" } satisfies Erc20Action))
-            .result,
+    await readValues(contract);
+
+    console.log("\n=== set message ===");
+
+    await contract.writeInteraction(
+        { function: "setMessage", message: "hello" } satisfies ContractAction,
+        { strict: true },
     );
 
-    console.log("=== funding ===");
+    await readValues(contract);
 
-    await erc20.writeInteraction({ function: "fundMe" } satisfies Erc20Action, { strict: true });
+    console.log("\n=== overwrite message ===");
 
-    console.log("=== transfering to bob ===");
-
-    await proxy.writeInteraction({
-        function: "transfer",
-        target: "bob",
-        quantity: 100,
-        erc20: erc20TxId,
-    } satisfies ProxyAction);
-
-    console.log((await erc20.viewState({ function: "balanceOf" } satisfies Erc20Action)).result);
-    console.log(
-        (await erc20.viewState({ function: "balanceOf", target: "bob" } satisfies Erc20Action))
-            .result,
+    await contract.writeInteraction(
+        { function: "setMessage", message: "world" } satisfies ContractAction,
+        { strict: true },
     );
 
-    console.log("=== transfering from bob ===");
+    await readValues(contract);
 
-    await proxy.writeInteraction({
-        function: "transfer",
-        from: "bob",
-        target: wallet1Addr,
-        quantity: 100,
-    } satisfies Erc20Action);
+    console.log("\n=== remove message ===");
 
-    console.log("=== reading result ===");
+    await contract.writeInteraction({ function: "removeMessage" } satisfies ContractAction, {
+        strict: true,
+    });
 
-    console.log((await erc20.viewState({ function: "balanceOf" } satisfies Erc20Action)).result);
-    console.log(
-        (await erc20.viewState({ function: "balanceOf", target: "bob" } satisfies Erc20Action))
-            .result,
-    );
+    await readValues(contract);
 })();
